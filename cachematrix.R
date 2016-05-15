de94f40ee177324b9521602f13dd1620225ac668
KristinaGilmour <kogilmour@gmail.com>
2016-05-15 13:50:23 -0700

    Included functions for comuting and cahing inverese of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Computing  and caching inverse of a matrix. 
 
-## Write a short comment describing this function
+# Creates list of functions which include methods to set and get values of matrix, 
+# and set and get cached inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() {x}
+    setinv <- function(solve) {inv <<- solve}
+    getinv <- function() {inv}
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# Returns inverse of a matrix from cache. 
+# If cache is empty calculates and returns inverse, and updates cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("Getting cached data")
+        return(inv)
+    }
+    matx <- x$get()
+    inv <- solve(matx, ...)
+    x$setinv(inv)
+    inv
 }
+
+
